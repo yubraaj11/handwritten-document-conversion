e1b36975859c355467bb80d8076495225f434389
yubraaj11 <yubrajsigdel1@gmail.com>
2024-09-19 00:34:45 +0545

    Pipeline's main function added

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+
+from src.handwritten_document_conversion.text_recognition import TextRecognition
+from src.handwritten_document_conversion.text_detection import TextDetection
+
+
+file_path = os.path.dirname(os.path.abspath(__file__))
+OG_IMG_DIR = os.path.join(file_path,  'images', 'original')
+RESIZED_IMG_DIR = os.path.join(file_path, 'images', 'resized')
+TEXT_FILE_DIR = os.path.join(file_path, 'txt')
+
+IMG_FILE = '82.jpg'
+
+def pipeline_function():
+    text_det_obj = TextDetection(image_file=IMG_FILE)
+    text_rec_obj = TextRecognition()
+
+    _, cropped_images_file_name = text_det_obj.return_cropped_images()
+
+    texts = []
+    for cropped_image in cropped_images_file_name:
+        generated_text = text_rec_obj.return_generated_text(image=os.path.join(RESIZED_IMG_DIR, cropped_image))
+        texts.append(generated_text)
+
+
+    with open(os.path.join(TEXT_FILE_DIR, "predicted_82.txt"), 'w') as file:
+        for text in texts:
+            file.write(text)
+            file.write(' ')
+
+if __name__ == "__main__":
+    pipeline_function()
