1c2d07bfe8220dafcd23df37b58cc9e547f4de23
yubraaj11 <yubrajsigdel1@gmail.com>
2024-09-19 09:51:01 +0545

    Pipeline function to detect and recognize text and save the .txt file is added

--- main.py
+++ main.py
@@ -3,16 +3,13 @@ import os
 from src.handwritten_document_conversion.text_recognition import TextRecognition
 from src.handwritten_document_conversion.text_detection import TextDetection
 
-
 file_path = os.path.dirname(os.path.abspath(__file__))
 OG_IMG_DIR = os.path.join(file_path,  'images', 'original')
 RESIZED_IMG_DIR = os.path.join(file_path, 'images', 'resized')
 TEXT_FILE_DIR = os.path.join(file_path, 'txt')
 
-IMG_FILE = '82.jpg'
-
-def pipeline_function():
-    text_det_obj = TextDetection(image_file=IMG_FILE)
+def pipeline_function(img_file):
+    text_det_obj = TextDetection(image_file=img_file)
     text_rec_obj = TextRecognition()
 
     _, cropped_images_file_name = text_det_obj.return_cropped_images()
@@ -23,10 +20,11 @@ def pipeline_function():
         texts.append(generated_text)
 
 
-    with open(os.path.join(TEXT_FILE_DIR, "predicted_82.txt"), 'w') as file:
+    with open(os.path.join(TEXT_FILE_DIR, "predicted_cahana.txt"), 'w') as file:
         for text in texts:
             file.write(text)
             file.write(' ')
 
+
 if __name__ == "__main__":
-    pipeline_function()
+    pipeline_function(img_file='cahana.jpg')
