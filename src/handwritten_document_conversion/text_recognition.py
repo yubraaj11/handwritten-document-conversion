a88134941773afc183bfb3407995ffa48ecc8d27
yubraaj11 <yubrajsigdel1@gmail.com>
2024-10-07 10:41:21 +0545

    Model and pixel values are put into cuda

--- src/handwritten_document_conversion/text_recognition.py
+++ src/handwritten_document_conversion/text_recognition.py
@@ -4,6 +4,7 @@ from PIL import Image
 from transformers import AutoTokenizer, ViTImageProcessor, TrOCRProcessor, VisionEncoderDecoderModel
 from dotenv import load_dotenv
 import numpy as np
+import torch  # Import PyTorch for device handling
 
 load_dotenv()
 
@@ -16,6 +17,7 @@ class TextRecognition:
     _model = None
     _feature_extractor = None
     _processor = None
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # Check for GPU availability
 
     def __init__(self):
         if TextRecognition._tokenizer is None:
@@ -24,6 +26,7 @@ class TextRecognition:
         if TextRecognition._model is None:
             TextRecognition._model = VisionEncoderDecoderModel.from_pretrained(MODEL_NAME)
             TextRecognition._model.config.early_stopping = True
+            TextRecognition._model.to(TextRecognition.device)  # Move the model to the specified device
 
         if TextRecognition._feature_extractor is None:
             TextRecognition._feature_extractor = ViTImageProcessor.from_pretrained(FEATURE_EXTRACTOR)
@@ -32,7 +35,6 @@ class TextRecognition:
             TextRecognition._processor = TrOCRProcessor(feature_extractor=TextRecognition._feature_extractor,
                                                         tokenizer=TextRecognition._tokenizer
                                                         )
-
 
     @staticmethod
     def return_generated_text(image_path: str) -> str:
@@ -43,17 +45,19 @@ class TextRecognition:
         """
 
         # Convert OpenCV image (NumPy array) to PIL Image
-        # pil_image = cv2.imread(image_path)
         pil_image = Image.open(image_path)
 
         if TextRecognition._processor is None:
             raise ValueError("Processor is not initialized.")
 
-        # Process the image and generate text
+        # Process the image
         pixel_values = TextRecognition._processor(pil_image, return_tensors="pt").pixel_values
-        generated_ids = TextRecognition._model.generate(pixel_values,
-                                                        early_stopping=True)  # Pass early_stopping=True
+
+        # Move pixel values to the specified device
+        pixel_values = pixel_values.to(TextRecognition.device)
+
+        # Generate text
+        generated_ids = TextRecognition._model.generate(pixel_values, early_stopping=True)
         generated_text = TextRecognition._processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
         return generated_text
-
